270d10e67cfff981e417a520928b6362ab45e969
zulqernain <zulqernainabbasi@gmail.com>
2026-05-16 19:18:58 +0500

    Hero-Section

--- LuxuryStay/src/App.jsx
+++ LuxuryStay/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import {Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
   return(
     <div>
-      <Navbar/>
+      {!isOwnerPath && <Navbar/>}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </div>
     </div>
   )
 }
